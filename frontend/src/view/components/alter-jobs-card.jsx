51e2566bffa752339baafa2e238c60f59b8511a3
Vo Van Khanh <khanh606m@gmail.com>
2025-10-19 20:43:40 +0700

    All done

--- frontend/src/view/components/alter-jobs-card.jsx
+++ frontend/src/view/components/alter-jobs-card.jsx
@@ -1,17 +1,20 @@
 import './style/alterjobs.css';
+import { Link } from 'react-router-dom';
 function AlterJobsCard({id, title, img, onSelect}) {
     return (
         <div className="alter-jobs-header">
             <h1>{title}</h1>
             <img src={img} alt="Jobs Banner" className="alter-jobs-banner" />
-            <button
-                type="button"
-                data-id={id}
-                onClick={() => onSelect?.(id)}
-                aria-label={`Learn more about ${title}`}
-            >
-                Learn More
-            </button>
+            <Link to="/jobs">
+                <button
+                    type="button"
+                    data-id={id}
+                    onClick={() => onSelect?.(id)}
+                    aria-label={`Learn more about ${title}`}
+                >
+                    Learn More
+                </button>
+            </Link>
         </div>
     );
 }
